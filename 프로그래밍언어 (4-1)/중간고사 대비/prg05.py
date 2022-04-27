86db667cecb33224b65de868c7808bac2e1b1c92
Sojeong <dlthwjd6176@gmail.com>
2022-04-27 23:40:16 +0900

    commit_07

--- 프로그래밍언어 (4-1)/중간고사 대비/prg05.py
+++ 프로그래밍언어 (4-1)/중간고사 대비/prg05.py
@@ -1,2 +1,12 @@
 #4-1확장형 데이터
+sum2=0
+data=list(map(int,input("데이터 입력==>").split(" "))) #split(" ")- 나누기 자르기
+print(data)
+
+avg= sum (data)/len(data)
+for val in data:
+    sum2 += (val - avg) ** 2 #**- 제곱
+print("평균:%5.3f, 합계: %5d" % (avg,sum(data)))
+print("최댓값:%d,최솟값:%d,범위:%d"%(max(data), min(data),max(data)-min(data)))
+print("분산 : %10.4f"% (sum2/len(data)))
 
--- 프로그래밍언어 (4-1)/중간고사 대비/prg07.py
+++ 프로그래밍언어 (4-1)/중간고사 대비/prg07.py
@@ -1,8 +1,13 @@
-#4-4
 matrix = [[10,20,30],
           [40,50,60],
           [70,80,90]]
 t_matrix=[[0,0,0],
           [0,0,0],
           [0,0,0]]
-print(matrix)
+
+for row in range(len(matrix)):   #len(matrix)->3
+    for col in range(len(matrix[row])):
+        t_matrix[col][row] = matrix[row][col]
+print("원본행렬=", matrix)
+print("전치행렬=", t_matrix)
+t1=[ ]
